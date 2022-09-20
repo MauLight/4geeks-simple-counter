b826b88b5478f625fbee0a8e6f4afcb50c90f1a2
MauLight <maulisseluz@gmail.com>
2022-09-20 10:40:55 -0300

    basic functionality working

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,7 +4,7 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
 		<div className="text-center">
 			<h1 className="text-center mt-5">Hello Rigo!</h1>
@@ -12,7 +12,7 @@ const Home = () => {
 				<img src={rigoImage} />
 			</p>
 			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
+				{props.num}
 			</a>
 			<p>
 				Made by{" "}
--- src/js/index.js
+++ src/js/index.js
@@ -8,5 +8,55 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+const SimpleCounter = (props) => {
+
+
+    return (
+        <div className="wrapper">
+            <div className="clock btn btn-dark" onClick={resetCounter} onDoubleClick={reloadCounter}>
+                <i className="far fa-clock"></i>
+            </div>
+            <div className="btn btn-dark">{props.day}</div>
+            <div className="btn btn-dark">{props.hour}</div>
+            <div className="btn btn-dark">{props.min}</div>
+            <div className="btn btn-dark">{props.sec}</div>
+        </div>
+    )
+}
+
+let time = 0;
+let sec = 1;
+let min = 0;
+let hour = 0;
+let day = 0;
+
+const intervalID = setInterval(() => {
+    sec = time + 1;
+
+    if (sec === 59) {
+        time = -2;
+    }
+
+    if (sec == 0) {
+        min += 1
+    };
+    if (min == 60) {
+        return hour += 1
+    };
+    if (hour == 24) {
+        return day += 1
+    };
+    time++;
+    console.log(sec, min, hour, day);
+    ReactDOM.render(<SimpleCounter sec={sec} min={min} hour={hour} day={day} />, document.querySelector("#app"));
+
+
+}, 1000);
+
+const resetCounter = () => {
+    clearInterval(intervalID);
+};
+
+const reloadCounter = () => {
+    window.location.reload();
+}
